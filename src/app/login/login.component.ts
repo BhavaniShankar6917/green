579aaed7f6dc140bc24374b75b31fe280c27e54a
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-08-10 10:40:46 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,36 @@
-import { SupabaseService } from './../supabase.service';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SupabaseService } from "./../supabase.service";
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
+// import { Server } from "worker/deno";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
-  title = 'Login';
+  title = "Login";
   constructor(private supabase: SupabaseService) {}
+
   signup = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+  });
+  newUser: boolean = true;
+  login = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
   });
   signupWithEmail(email: string, password: string) {
     this.supabase.registerUser(email, password);
   }
+  loginWithEmail(email: string, password: string) {
+    this.supabase.signin(email, password);
+  }
   get emailValue() {
-    return this.signup.get('email')?.value as string;
+    return this.signup.get("email")?.value as string;
   }
   get passwordValue() {
-    return this.signup.get('password')?.value as string;
+    return this.signup.get("password")?.value as string;
   }
 }
