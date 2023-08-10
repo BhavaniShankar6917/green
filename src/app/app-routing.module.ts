537554f20ac0fe128aa9666b303e94db1c424330
BhavaniShankar6917 <91416094+BhavaniShankar6917@users.noreply.github.com>
2023-08-10 10:49:08 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+                         { path: "feed", component: MainComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
